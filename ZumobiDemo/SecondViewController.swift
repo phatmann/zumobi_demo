0f6db45ae03e270e2e3d602a380a7e98dac90adb
Tony Mann <thephatmann@gmail.com>
2015-04-23 09:49:45 -0600

    Initial Commit

---
+++ ZumobiDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ZumobiDemo
+//
+//  Created by Tony Mann on 4/23/15.
+//  Copyright (c) 2015 7Actions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
