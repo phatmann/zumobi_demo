8dea28a5a5803feaba6bcc9880d5fbaefa89a5c4
Tony Mann <thephatmann@gmail.com>
2015-04-23 11:06:56 -0600

    Skeleton in place

---
+++ ZumobiDemo/ModalDemoViewController.swift
@@ -0,0 +1,11 @@
+//  Created by Tony Mann on 4/23/15.
+//  Copyright (c) 2015 7Actions. All rights reserved.
+
+import UIKit
+
+class ModalDemoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
